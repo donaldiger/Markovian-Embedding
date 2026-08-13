ec8073eff6f6c4060dcde77e37d10a614bfe0619
Mathis Vernier <mathis.vernier@polytechnique.edu>
2026-08-13 16:20:41 +0200

    linearity of series check +  polish linear memory effect detection

--- scripts/main/main_pca_var.py
+++ scripts/main/main_pca_var.py
@@ -2,7 +2,14 @@ from src.utils import DataLoader
 
 from src.var     import VAR
 
+import pandas as pd
+import numpy as np
+
+
+
 if __name__ == "__main__":
+    data = pd.read_csv("/Users/mathisvernier/Markovian-Embedding/data/processed/processed_FRED_MD.csv")
+
     # Load data
     # Apply PCA
     # Train VAR model
